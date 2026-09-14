f1e03de4ee06ec2c12066afef7cc35d37a9502c3
Pascalls95 <rohanprasenna@yahoo.com>
2026-09-14 18:39:59 +1000

    got mixed up with names, javascript is for manual input

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-//text area recipe input
+//manual recipe input
 document.getElementById("examineRecipe").addEventListener("click", () => {
     const container = document.createElement("div");
     container.classList.add("ingredient");
+
+    container.innerHTML = `
+        <input class="name" type="text" placeholder="Ingredient Name">
+
+    `
+    ;
 })
