87fe0b563262aafd8bb621c182bbca9a77b72164
Pascalls95 <rohanprasenna@yahoo.com>
2026-09-14 18:37:35 +1000

    start of javascript for text area input

---
+++ script.js
@@ -0,0 +1,5 @@
+//text area recipe input
+document.getElementById("examineRecipe").addEventListener("click", () => {
+    const container = document.createElement("div");
+    container.classList.add("ingredient");
+})
